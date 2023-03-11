6c0f9a845c4f981ec2b1ac9905c8ac5a132d931e
CGHSRobotics <cghsrobotics@gmail.com>
2023-03-10 20:06:27 -0500

    Co-authored-by: Nick-Admin <Nick-Admin@users.noreply.github.com>

---
+++ src/ace.h
@@ -0,0 +1,190 @@
+
+#ifndef ACE_CGHS
+#define ACE_CGHS
+
+#include "main.h"
+
+
+/* ========================================================================== */
+/*                             Main Ace Namespace                             */
+/* ========================================================================== */
+namespace ace {
+
+
+	/* ========================================================================== */
+	/*                                Device Ports                                */
+	/* ========================================================================== */
+
+	/* --------------------------------- Chassis -------------------------------- */
+
+	#define PORT_CHASSIS_L_F 1
+	#define PORT_CHASSIS_L_C 2
+	#define PORT_CHASSIS_L_B 3
+
+	#define PORT_CHASSIS_R_F 4
+	#define PORT_CHASSIS_R_C 5
+	#define PORT_CHASSIS_R_B 6
+
+
+	/* ------------------------- Other Motors / Devices ------------------------- */
+	#define PORT_INTAKE 7
+	#define PORT_LAUNCHER 8
+
+	#define PORT_VISION 9
+	#define PORT_IMU 10
+
+
+	/* ========================================================================== */
+	/*                                   Buttons                                  */
+	/* ========================================================================== */
+
+	#define BTN_INTAKE_TOGGLE  DIGITAL_L1
+	#define BTN_LAUNCH_DISKS DIGITAL_R2
+	#define BTN_ENDGAME_TOGGLE DIGITAL_UP
+	#define BTN_CONVEYOR_FWD DIGITAL_LEFT
+	#define BTN_CONVEYOR_REVERSE DIGITAL_RIGHT
+	#define BTN_ROLLER_FWD DIGITAL_A
+	#define BTN_ROLER_REVERSE DIGITAL_B
+	#define INTAKE_REVERSE DIGITAL_L2
+	#define BTN_FLAP_TOGGLE DIGITAL_A
+	#define BTN_ROLLER_LAUNCHER_REVERSE DIGITAL_R1
+
+
+
+	/* ========================================================================== */
+	/*                              Global Variables                              */
+	/* ========================================================================== */
+
+	/* ------------------------------ Miscellaneuos ----------------------------- */
+
+
+	/* ------------------------------- SPEEEEEEED ------------------------------- */
+
+
+	/* ========================================================================== */
+	/*                      Device Declaration / Definitions                      */
+	/* ========================================================================== */
+
+	/* --------------------------------- Chassis -------------------------------- */
+
+	// Wheel diameter
+	const okapi::QLength chassis_wheel_diameter = 3.25_in;
+
+	const okapi::QLength chassis_wheel_track = 11.5_in;
+
+	const std::shared_ptr<okapi::ChassisController> chassis =
+		okapi::ChassisControllerBuilder()
+		.withMotors(
+			{ PORT_CHASSIS_L_F, PORT_CHASSIS_L_C, PORT_CHASSIS_L_B },
+			{ PORT_CHASSIS_R_F, PORT_CHASSIS_R_C, PORT_CHASSIS_R_B }
+		)
+		.withDimensions(okapi::AbstractMotor::gearset::blue, { {chassis_wheel_diameter, chassis_wheel_track}, imev5GreenTPR })
+		.build();
+
+
+	/* ------------------------- Other Motors / Devices ------------------------- */
+
+	// Launcher motor
+	const pros::Motor launcherMotor(PORT_LAUNCHER, MOTOR_GEARSET_06, false);
+
+	// Motor for intake, roller, and DTS
+	const pros::Motor intakeMotor(PORT_INTAKE, MOTOR_GEARSET_06, false);
+
+
+	//Vision sensor
+	const pros::Vision  visionSensor(PORT_VISION);
+
+	//
+	const pros::IMU imuSensor(PORT_IMU);
+
+	const pros::ADIDigitalOut endgamePneumatics(false);
+
+	/* ========================================================================== */
+	/*                            Function Declarations                           */
+	/* ========================================================================== */
+
+}
+
+
+/* ========================================================================== */
+/*                             Ace Util Namespace                             */
+/* ========================================================================== */
+namespace ace::util {
+
+
+
+
+
+
+
+}
+
+
+/* ========================================================================== */
+/*                             Ace Auton Namepace                             */
+/* ========================================================================== */
+namespace ace::auton {
+
+
+
+
+
+
+
+}
+
+
+/* ========================================================================== */
+/*                            Ace Launch Namespace                            */
+/* ========================================================================== */
+namespace ace::launch {
+
+
+
+
+
+
+
+}
+
+
+/* ========================================================================== */
+/*                             Ace LVGL Namespace                             */
+/* ========================================================================== */
+namespace ace::lvgl {
+
+
+
+
+
+
+
+}
+
+/*
+I can't do this!
+
+
+Vanessa, pull yourself together.
+You have to snap out of it!
+
+
+You snap out of it.
+
+
+You snap out of it.
+
+
+- You snap out of it!
+- You snap out of it!
+
+
+- You snap out of it!
+- You snap out of it!
+
+
+- You snap out of it!
+- You snap out of it!
+
+*/
+#endif 
